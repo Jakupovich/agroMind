25639142ed88c740c35bcbf24aa3795c971604a6
Norda <80918166+Jakupovich@users.noreply.github.com>
2026-04-20 16:59:04 +0200

    Update types.ts

--- theme/ui/types.ts
+++ theme/ui/types.ts
@@ -1,6 +1,6 @@
 // @ts-nocheck
+
 export interface AlertButton {
-<<<<<<< HEAD
   text: string;
   onPress?: () => void;
   style?: 'default' | 'cancel' | 'destructive';
@@ -12,16 +12,3 @@ export interface AlertState {
   message: string;
   buttons: AlertButton[];
 }
-=======
-    text: string;
-    onPress?: () => void;
-    style?: 'default' | 'cancel' | 'destructive';
-  }
-  
-  export interface AlertState {
-    visible: boolean;
-    title: string;
-    message: string;
-    buttons: AlertButton[];
-  }
->>>>>>> fa1781c314271bc6225f3f556fc8cc84d76e834a
