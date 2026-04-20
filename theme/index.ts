64783e16ad0bb0d6a4c4763ce0f8634cabad9a99
Norda <80918166+Jakupovich@users.noreply.github.com>
2026-04-20 17:00:08 +0200

    Update index.ts

--- theme/index.ts
+++ theme/index.ts
@@ -2,10 +2,7 @@
 export * from './core';
 export * from './auth/mock';
 export * from './auth/supabase';
+
 // UI exports
 export { useAlert, AlertProvider } from './ui';
-<<<<<<< HEAD
 export type { AlertButton, AlertState } from './ui';
-=======
-export type { AlertButton, AlertState } from './ui';
->>>>>>> fa1781c314271bc6225f3f556fc8cc84d76e834a
