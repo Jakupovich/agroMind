5d23577732600aaea885baa55ade3cb0b903d521
Norda <80918166+Jakupovich@users.noreply.github.com>
2026-04-20 16:54:24 +0200

    Update index.ts

--- theme/auth/mock/index.ts
+++ theme/auth/mock/index.ts
@@ -2,8 +2,4 @@
 export * from './hook';
 export * from './service';
 export * from './router';
-<<<<<<< HEAD
 export * from './context';
-=======
-export * from './context';
->>>>>>> fa1781c314271bc6225f3f556fc8cc84d76e834a
